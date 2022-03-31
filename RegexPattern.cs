490847df18bcedfe8f80dc6723390d02daf6eb90
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-03-31 02:33:14 +0200

    added regex for start and end time

---
+++ RegexPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace Queslar_Cave_Log_Analyzer
+{
+    internal static class RegexPattern
+    {
+        public static Regex startTime = new Regex("(?<!-.)[a-z]{3}\\s+[0-9]{1,2}.[0-9]{4}..[0-9:0-9]*", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+        public static Regex endTime = new Regex("-\\s?[a-z]{3}\\s+[0-9]{1,2}.[0-9]{4}..[0-9:0-9]*", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+    }
+}
