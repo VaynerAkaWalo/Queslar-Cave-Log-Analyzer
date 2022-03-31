9aadd68886b2e639344912eca120d167b5d02000
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-03-31 02:34:32 +0200

    added analyzer class

---
+++ Analyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queslar_Cave_Log_Analyzer
+{
+    internal class Analyzer
+    {
+        public static void Analyze()
+        {
+
+        }
+    }
+}
