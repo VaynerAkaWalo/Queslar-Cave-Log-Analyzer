c3b63481500040d541bf221210c9dc9b709c09a5
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-04-03 01:25:47 +0200

    added all loot properties

--- Report.cs
+++ Report.cs
@@ -121,32 +121,157 @@ namespace Queslar_Cave_Log_Analyzer
 
         #region Stats
 
+        private ulong strength;
+
+        public ulong Strength
+        {
+            get { return strength; }
+            set { strength = value; }
+        }
+
+        private ulong health;
+
+        public ulong Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        private ulong agility;
+
+        public ulong Agility
+        {
+            get { return agility; }
+            set { agility = value; }
+        }
+
+        private ulong dexterity;
+
+        public ulong Dexterity
+        {
+            get { return dexterity; }
+            set { dexterity = value; }
+        }
+
+
+
         #endregion
 
         #region Monster Stats
 
+        private ulong monster_strength;
+
+        public ulong Monster_strenth
+        {
+            get { return monster_strength; }
+            set { monster_strength = value; }
+        }
+
+        private ulong monster_health;
+
+        public ulong Monster_health
+        {
+            get { return monster_health; }
+            set { monster_health = value; }
+        }
+
+        private ulong monster_agility;
+
+        public ulong Monster_Agility
+        {
+            get { return monster_agility; }
+            set { monster_agility = value; }
+        }
+
+        private ulong monster_dexterity;
+
+        public ulong Monster_dexterity
+        {
+            get { return monster_dexterity; }
+            set { monster_dexterity = value; }
+        }
+
         #endregion
 
         #region Wootz
-        private int rareWootz;
 
-        public int RareWootz
+        private int rare_wootz;
+
+        public int Rare_wootz
         {
-            get { return rareWootz; }
-            set { rareWootz = value; }
+            get { return rare_wootz; }
+            set { rare_wootz = value; }
         }
 
         private int epicWootz;
 
-        public int EpicWootz    
+        public int Epic_wootz    
         {
             get { return epicWootz; }
             set { epicWootz = value; }
         }
 
+        private int magical_wootz;
+
+        public int Magical_wootz
+        {
+            get { return magical_wootz; }
+            set { magical_wootz = value; }
+        }
+
+        private int unique_wootz;
+
+        public int Unique_wootz
+        {
+            get { return unique_wootz; }
+            set { unique_wootz = value; }
+        }
+
+        private int relic_wootz;
+
+        public int Relic_wootz
+        {
+            get { return relic_wootz; }
+            set { relic_wootz = value; }
+        }
+
+
         #endregion
 
         #region Materials
+
+        private int frog_toe;
+
+        public int Frog_toe
+        {
+            get { return frog_toe; }
+            set { frog_toe = value; }
+        }
+
+        private int dog_tongue;
+
+        public int Dog_tongue
+        {
+            get { return dog_tongue; }
+            set { dog_tongue = value; }
+        }
+
+        private int lizard_leg;
+
+        public int Lizard_leg
+        {
+            get { return lizard_leg; }
+            set { lizard_leg = value; }
+        }
+
+        private int wolf_tooth;
+
+        public int Wolf_tooth
+        {
+            get { return wolf_tooth; }
+            set { wolf_tooth = value; }
+        }
+
 
         #endregion
 
@@ -154,24 +279,152 @@ namespace Queslar_Cave_Log_Analyzer
 
         #region T1
 
+        private ulong coal;
+
+        public ulong Coal
+        {
+            get { return coal; }
+            set { coal = value; }
+        }
+
+        private ulong wheat;
+
+        public ulong Wheat
+        {
+            get { return wheat; }
+            set { wheat = value; }
+        }
+
+        private ulong oak;
+
+        public ulong Oak
+        {
+            get { return oak; }
+            set { oak = value; }
+        }
+
+        private ulong shrimp;
+
+        public ulong Shrimp
+        {
+            get { return shrimp; }
+            set { shrimp = value; }
+        }
+
         #endregion
 
         #region T2
+
+        private ulong copper;
+
+        public ulong Copper
+        {
+            get { return copper; }
+            set { copper = value; }
+        }
+
+        private ulong dinkel;
+
+        public ulong Dinkel
+        {
+            get { return dinkel; }
+            set { dinkel = value; }
+        }
+
+        private ulong willow;
+
+        public ulong Willow
+        {
+            get { return willow; }
+            set { willow = value; }
+        }
+
+        private ulong trout;
+
+        public ulong Trout
+        {
+            get { return trout; }
+            set { trout = value; }
+        }
+
 
         #endregion
 
         #region T3
 
+        private ulong tin;
+
+        public ulong Tin
+        {
+            get { return tin; }
+            set { tin = value; }
+        }
+
+        private ulong khroasan;
+
+        public ulong Khroasan
+        {
+            get { return khroasan; }
+            set { khroasan = value; }
+        }
+
+        private ulong teak;
+
+        public ulong Teak
+        {
+            get { return teak; }
+            set { teak = value; }
+        }
+
+        private ulong lobster;
+
+        public ulong Lobster
+        {
+            get { return lobster; }
+            set { lobster = value; }
+        }
+
         #endregion
 
         #region T4
 
-        #endregion
+        private ulong silver;
+
+        public ulong Silver
+        {
+            get { return silver; }
+            set { silver = value; }
+        }
+
+        private ulong corn;
+
+        public ulong Corn
+        {
+            get { return corn; }
+            set { corn = value; }
+        }
+
+        private ulong maple;
+
+        public ulong Maple
+        {
+            get { return maple; }
+            set { maple = value; }
+        }
+
+        private ulong bass;
+
+        public ulong Bass
+        {
+            get { return bass; }
+            set { bass = value; }
+        }
 
         #endregion
 
         #endregion
 
+        #endregion
 
         #endregion
 
@@ -214,8 +467,6 @@ namespace Queslar_Cave_Log_Analyzer
             MatchCollection startingdepthMC = RegexPattern.startingDepth.Matches(log);
             MatchCollection finishingdepthMC = RegexPattern.finishingDepth.Matches(log);
 
-            MatchCollection diamondsMC = RegexPattern.diamonds.Matches(log);
-            MatchCollection goldMC = RegexPattern.gold.Matches(log);
 
             if(startMC.Count == endMC.Count)
             {
@@ -237,6 +488,20 @@ namespace Queslar_Cave_Log_Analyzer
                         TotalDepthDiff++;
                     }
                 }
+
+                TreasuresFound = Sumint(RegexPattern.treasuresFound, log);
+
+                BossesSlain = Sumint(RegexPattern.bossesSlain, log);
+
+                MonstersSlain = Sumint(RegexPattern.monstersSlain, log);
+
+                TilesTraveled = Sumint(RegexPattern.tilesTraveled, log);
+
+                TilesCleared = Sumint(RegexPattern.tilesCleared, log);
+
+                TilesLooted = Sumint(RegexPattern.tilesLooted, log);
+
+
 
                 Diamonds = Sumint(RegexPattern.diamonds, log);
 
