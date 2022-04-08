69559ae46cf4593c88dbf8656736f786a3c795be
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-04-09 00:42:14 +0200

    class for formatting output

---
+++ ConsoleWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Queslar_Cave_Log_Analyzer
+{
+    internal class ConsoleWriter
+    {
+
+        const string numberFormat = "#,##0.##";
+        const int padding = 18;
+        const int headerPadding = 15;
+
+        private readonly Action<string> _outputProvider;
+
+        public ConsoleWriter(Action<string> outputProvider)
+        {
+            _outputProvider = outputProvider;
+        }
+
+        public void PrintResult(string header, ulong item, double totalTime)
+        {
+            _outputProvider($"{header.PadRight(headerPadding)} | Total: {item.ToString(numberFormat).PadLeft(padding)} | Per hour: {(item / totalTime).ToString(numberFormat).PadLeft(padding)} | Per day: {(item / totalTime * 24).ToString(numberFormat).PadLeft(padding)} | ");
+        }
+    }
+}
